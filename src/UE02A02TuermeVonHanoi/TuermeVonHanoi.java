8c0586ba8ebd9e9c100fba8a00cb2664fb4c66a4
David <s57343@beuth-hochschule.de>
2017-10-23 14:55:06 +0200

    Init Türme von Hanoi

---
+++ src/UE02A02TuermeVonHanoi/TuermeVonHanoi.java
@@ -0,0 +1,21 @@
+package UE02A02TuermeVonHanoi;
+
+public class TuermeVonHanoi {
+    static void ziehe_scheibe(int nummer, String von, String nach) {
+        System.out.println("Scheibe " + nummer + " wird von " + von +
+                " nach " + nach + " verschoben ");
+    }
+    static void hanoi(int N, String platz1, String hilfsplatz, String platz2)
+    {
+        if (N == 1) {
+            ziehe_scheibe(N, platz1, platz2);
+        } else {
+            hanoi(N-1, platz1, platz2, hilfsplatz);
+            ziehe_scheibe(N, platz1, platz2);
+            hanoi(N-1, hilfsplatz, platz1, platz2);
+        }
+    }
+    public static void main(String[] args) {
+        hanoi(4, "A", "B", "C");
+    }
+}
